d08d008f510248f0d8f92aa1f624f3e86060f057
claudneysessa <claudneysartisessa@gmail.com>
2024-11-19 12:53:54 -0300

    Versão: 3.19.0

--- example/android/app/src/main/kotlin/com/example/flutter_create_plugin_versions_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/flutter_create_plugin_versions_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.flutter_create_plugin_versions_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
