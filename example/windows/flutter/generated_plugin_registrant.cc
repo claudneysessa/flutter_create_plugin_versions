c431828b2289e8c7c3a2c5e16664304e23d6ca84
claudneysessa <claudneysartisessa@gmail.com>
2024-11-19 12:45:40 -0300

    Versão: 2.10.0

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_create_plugin_versions/flutter_create_plugin_versions_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterCreatePluginVersionsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterCreatePluginVersionsPlugin"));
+}
